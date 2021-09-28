b382162cd3f27240e95acbe3012069430b644485
cherinda30 <91549106+cherinda30@users.noreply.github.com>
2021-09-28 21:48:41 +0700

    init

--- elemen kompetensi 3.py
+++ elemen kompetensi 3.py
@@ -4,5 +4,5 @@ Created on Tue Sep 28 21:20:18 2021
 
 @author: Cherinda maharani
 """
-kalimat = input("masukkan kalimat :")
-print(kalimat.upper())
+kata = input("masukkan kata :")
+print(kata.upper())
